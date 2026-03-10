3c3c7537203770646d6d85a0a3c33779ce1b2d9c
Swanand Bowalekar <swanand264@gmail.com>
2026-03-10 22:05:24 +0530

    Add BrowserStack execution screenshot and update driver setup for remote WebDriver

--- driver/driver_setup.py
+++ driver/driver_setup.py
@@ -1,12 +1,21 @@
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver import Remote
+from selenium.webdriver.chrome.options import Options
 
 def setup_driver():
-    options = webdriver.ChromeOptions()
 
-    driver = webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
+    options = Options()
+
+    options.set_capability("browserName", "Chrome")
+    options.set_capability("browserVersion", "latest")
+
+    options.set_capability("bstack:options", {
+        "sessionName": "El Pais Scraper Test",
+        "buildName": "browserstack-el-pais-scraper",
+        "projectName": "browserstack-assignment"
+    })
+
+    driver = Remote(
+        command_executor="https://hub-cloud.browserstack.com/wd/hub",
         options=options
     )
 
